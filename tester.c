a6da1299a7670fcb64b1c23e47bc2b4286ef29e7
imanirod <idrodney@ucdavis.edu>
2018-03-11 15:19:46 -0700

    working tester.c for compare function

--- tester.c
+++ tester.c
@@ -4,22 +4,28 @@
 
 int compare(const void *p1, const void *p2)
 {
-        const int (*a)[2] = p1;
-    const int (*b)[2] = p2;
-    if ( (*a)[0] < (*b)[0] ) return -1;
-    if ( (*a)[0] > (*b)[0] ) return +1;
+        const int a = *( const int **)p1;
+    const int b = *(const int **)p2;
+//printf("count2: %d\n", a);
+//printf("count2: %d\n", b);
+    if ( a > b ) return -1;
+    if ( a < b ) return +1;
     return 0;
 }
 
 
 int main(int argc, char *argv[]){
 
-int count[10][2] = { (1,0),(2,1),(3,2),(4,3),
-                (5,4),(4,5),(7,6),(8,7),(9,8),(11,9) };
-//qsort(count, 10*2, sizeof count[0], compare);
+int count[10][2] = { {-1,0},{-3,1},{-2,2},{-4,3},
+                {-5,4},{-4,5},{-17,6},{-8,7},{-9,8},{-11,9} };
+qsort(count, 10, sizeof (int*), compare);
+//printf("count1: %d\n", count[0][1]);
+//printf("count1: %d\n", count[1][1]);
+//compare(count[0],count[1]);
 for(int i = 0; i < 10; i++) {
+   printf("row: %d, ", count[i][1]);
    printf("Count: %d\n",count[i][0]);
 }
 return 0;
-	
+
 }
