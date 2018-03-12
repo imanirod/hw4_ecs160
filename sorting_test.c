c889facabbda9561bc6058504fb02323cd15213a
ktyoshioka <kt.yoshioka@att.net>
2018-03-11 17:19:28 -0700

    Added appropriate error messages,misc.

--- sorting_test.c
+++ sorting_test.c
@@ -17,6 +17,7 @@ int findName(char dictionary[][50], unsigned int dictionary_length, char* tok) {
   return -1;
 }
 
+// compare function for qsort
 int compare(const void *p1, const void *p2)
 {
     const int a = *( const int **)p1;
@@ -26,19 +27,18 @@ int compare(const void *p1, const void *p2)
     return 0;
 }
 
-
 // main
 int main(int argc, char *argv[]){
   //read file
   if(argc != 2){
-    printf("Invalid Command Line Args\n");
+    printf("Invalid Input Format\n");
     return 1; //no filename
   }
   
   int length_filename = strlen(argv[1]);
   
-  if(length_filename > 50){
-    printf("Invalid Filename Length\n");
+  if(length_filename > 100){
+    printf("Invalid Input Format\n");
     return 1; //filename too long
   }
   
@@ -49,9 +49,7 @@ int main(int argc, char *argv[]){
   int MAX_TWEETERS = 20000;
   int MAX_TWEETER_NAME_LENGTH = 50;
   char dictionary[MAX_TWEETERS][MAX_TWEETER_NAME_LENGTH];
-
   int count[MAX_TWEETERS][2];
-
   int last_valid_tweeter_index = -1;
   
   // initialise the tweeter counts to zero
@@ -63,7 +61,7 @@ int main(int argc, char *argv[]){
   //open file for reading
   FILE * f = fopen(filename, "r");
   if(f == NULL) {
-    printf("Invalid Input CSV Filename\n");
+    printf("Invalid Input Format\n");
     return 0;
   }
   
@@ -81,6 +79,7 @@ int main(int argc, char *argv[]){
   if(fgets(line, MAX_LINE_LENGTH,f) != NULL) {
     token = strtok(line,",");
 
+    // cycle through tokens
     while(token != NULL) {
       if (strcmp(token, "name") == 0 || strcmp(token,"\"name\"") == 0
           || strcmp(token, "name\n") == 0 || strcmp(token,"\"name\"\n") == 0)
@@ -91,20 +90,20 @@ int main(int argc, char *argv[]){
   
     // Check that we actually have columns in the CSV file
     if(numHeaders == 0) {
-      printf("Invalid: CSV file has no headers\n");
+      printf("Invalid Input Format\n");
       return 1; //no rows (i.e. headers) at all
     }
 
     // Check that there is a column called "name"
     if(name_index < 0) {
-      printf("Invalid: No \'Name\' attribute in CSV file\n");
+      printf("Invalid Input Format\n");
       return 1; // no column called "name"
      } 
    }
   
   // parse the rest of the file.
   // jumpstart the loop
-  // Loop thorugh file
+  // Loop thorugh file, read in lines
   while(fgets(line,MAX_LINE_LENGTH,f) != NULL) {
     // read in a token
     token = strtok(line, ",");
@@ -132,10 +131,10 @@ int main(int argc, char *argv[]){
       // check if token is second-to-last
       if(num_tok == numHeaders - 2) {
         token = strtok(NULL,"\n");
-        }
+       }
       else {
         token = strtok(NULL, ",");
-        }
+       }
 
       num_tok++;
     
@@ -144,26 +143,29 @@ int main(int argc, char *argv[]){
     // check that the there are the right number of tokens in the line
     // the current token should be null, and numTok should be equal to the number of headers
     if(token != NULL || num_tok != numHeaders) {
-        printf("columns don't match up with the number of headers\n");
+        printf("Invalid Input Format\n");
 	      return 1;
     }
   } // end outer while
 
   // Check if the file has headers but no data, last_valid_tweeter_index should be -1.  if so, return.
   if(last_valid_tweeter_index == -1) {
-    printf("Invalid: no data rows\n");
+    printf("Invalid Input Format\n");
     return 1;
   }
+	
+  // sort
   qsort(count, MAX_TWEETERS, sizeof (int*), compare);
-  // for loop for testing
+	
+  // for loop for printing
   for(int i = 0; i < MAX_TWEETERS; i++) {
     if(count[i][0] != 0) {
-      printf("Name: %s\n",dictionary[count[i][1]]);
-      printf("Count: %d\n",count[i][0]);
+      printf("%s:",dictionary[count[i][1]]);
+      printf("%d\n",count[i][0]);
     }
     else {
       break;
-      }
+    }
   }
   return 0;
 }
